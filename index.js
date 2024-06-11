22f1e9789399bc93cd8f80173d7492e13e099c1a
Francisco0304 <fgmorales2017@gmail.com>
2024-06-11 15:40:07 -0500

    yeah

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const cors = require('cors'); // Importar el middleware cors
 
-const app = express()
+const app = express();
 
-app.set('PORT',process.env.PORT || 3200)
+app.set('PORT', process.env.PORT || 3200);
 
 // Habilitar el middleware CORS
 const corsOptions = {
-  origin: 'https://backluis.vercel.app/',
+  origin: 'https://backluis.vercel.app', // Asegúrate de que este sea el origen correcto
   optionsSuccessStatus: 200 // Para algunos navegadores antiguos
 };
 
@@ -16,16 +16,16 @@ app.use(cors(corsOptions));
 
 app.use(express.static(__dirname + "/public/"));
 
-app.get('/',(req,res)=>{
-  res.sendFile(path.join(__dirname,'/views/index.html'))
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
 
 app.get('/book.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'book.html'));
+  res.sendFile(path.join(__dirname, 'views', 'book.html'));
 });
 
 app.get('/catalog.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'catalog.html'));
+  res.sendFile(path.join(__dirname, 'views', 'catalog.html'));
 });
 
 app.get('/loanreservation.html', (req, res) => {
@@ -36,4 +36,4 @@ app.get('/loan.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'loan.html'));
 });
 
-app.listen(app.get('PORT'),()=>console.log(`Server front in port ${app.get('PORT')}`))
+app.listen(app.get('PORT'), () => console.log(`Server front in port ${app.get('PORT')}`));
